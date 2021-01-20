3873b70ecef36da0c47e64b21f59083db7b55e80
Dima <39500760+Dima0018@users.noreply.github.com>
2021-01-20 20:09:43 +0200

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if(n<0){
+        n = 0 - n;
+    }
+    let array = n.toString().split("");
+    let res = "";
+    for(let i =array.length-1; i>-1; i--){
+        res += array[i]
+    }
+    return +res;
 }
